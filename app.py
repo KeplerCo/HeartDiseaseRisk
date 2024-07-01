8e7329f80df0de5ec53aa96202cf9dc01dd9a711
Vanherwegentim <DarkJaffaCake@gmail.com>
2024-07-01 18:47:09 +0200

    update

--- app.py
+++ app.py
@@ -747,19 +747,19 @@ if selected == "Dashboard":
             - Smoking: {smokingcat}
             - Stroke: {strokecat}"""
 
-    import enum
-    class BMICategory(enum.Enum):
-        skip_on_failure=True
-        Underweight = "Underweight (BMI < 18.5)"
-        NormalWeight = "Normal weight (18.5 <= BMI < 25.0)"
-        Overweight = "Overweight (25.0 <= BMI < 30.0)"
-        Obese = "Obese (30.0 <= BMI < +Inf)"
+    # import enum
+    # class BMICategory(enum.Enum):
+    #     skip_on_failure=True
+    #     Underweight = "Underweight (BMI < 18.5)"
+    #     NormalWeight = "Normal weight (18.5 <= BMI < 25.0)"
+    #     Overweight = "Overweight (25.0 <= BMI < 30.0)"
+    #     Obese = "Obese (30.0 <= BMI < +Inf)"
 
-    @tool
-    def filter_data_for_BMI_Category(bmi: BMICategory):
-        """Filters the dataset based on the BMI category and returns the amount of rows that satisfy those parameters."""
-        filtered_df = heart[heart["BMICategory"] == bmi.value]
-        return filtered_df.shape[0]
+    # @tool
+    # def filter_data_for_BMI_Category(bmi: BMICategory):
+    #     """Filters the dataset based on the BMI category and returns the amount of rows that satisfy those parameters."""
+    #     filtered_df = heart[heart["BMICategory"] == bmi.value]
+    #     return filtered_df.shape[0]
     
     @tool
     def sample_data_set():
